722390f295eb464fb9e06dab3750f649647ce642
natanaelmizzone <natanmizzone@gmail.com>
2025-11-26 22:23:03 -0300

    atual
    atualização

---
+++ 02/app.js
@@ -0,0 +1,20 @@
+let n1 = 5, n2 = 7, adicao = 0, subtracao = 0, divisao = 0, mutiplicacao = 0, modulo = 0, esponenciacao = 0
+
+adicao=n1+n2
+subtracao=n1-n2
+divisao=n1/n2
+mutiplicacao=n1*n2
+modulo=n1**n2
+esponenciacao=n1%n2
+
+console.log("seu resultado é " + adicao)
+console.log("seu resultado é " + subtracao)
+console.log("seu resultado é " + divisao)
+console.log("seu resultado é " + mutiplicacao)
+console.log("seu resultado é " + modulo)
+console.log("seu resultado é " + esponenciacao)
+
+
+
+
+
